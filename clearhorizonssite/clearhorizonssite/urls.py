6e8b073f51beac3c5d75005d0e5615ba61f468ad
computernate <nateroskelley@gmail.com>
2025-03-24 16:03:59 -0600

    the icon

--- clearhorizonssite/clearhorizonssite/urls.py
+++ clearhorizonssite/clearhorizonssite/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import RedirectView
+
 from .views import index
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,7 +9,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ch_base/', include('ch_base.urls')),  # Include your API URLs
-    re_path(r'^.*$', index)
+    re_path(r'^.*$', index),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
